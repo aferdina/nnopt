2abcbddd5afcdb712de878295365d9e968f132aa
aferdina <aferdina@mail.uni-mannheim.de>
2021-10-04 13:28:26 +0200

    up

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import dash_cytoscape as cyto
 import networkx as nx
 import plotly.graph_objects as go
 from networks import NetworkDOS
+from networks import NetworkeasyDOS
 import torch
 import corr_stopp
 
@@ -62,9 +63,9 @@ def update_graph_scatter(x):
         plot_nn.epoch = 0
         plot_nn.step += 1
     logger.trace("Play action.")
-    model = NetworkDOS(nb_stocks=1, hidden_size=4)
+    model = NetworkeasyDOS(nb_stocks=1, hidden_size=4)
     #generate non copied case
-    fpath = f"/Users/andreferdinand/Desktop/Coding2/output/neural_networks/phase_{plot_nn.step}/model_epoch_{plot_nn.epoch}.pt"
+    fpath = f"/Users/andreferdinand/Desktop/Coding2/output/neural_networkseasy/phase_{plot_nn.step}/model_epoch_{plot_nn.epoch}.pt"
     checkpoint = torch.load(fpath)
     model.load_state_dict(checkpoint['model_state_dict'])
     stock_values_test = np.reshape([1,2,3,4,5,6],(6,1))
@@ -79,7 +80,7 @@ def update_graph_scatter(x):
     #logger.debug(f"list of q values{np.round(model(input_test).detach().numpy().flatten().tolist())}")
     loss = checkpoint['loss']
     #logger.debug(f"true stopping times {corr_stopp.stoppingtimes[plot_nn.step+1,:].flatten()}")
-    if plot_nn.epoch ==28:
+    if plot_nn.epoch ==28:   
         logger.debug(f"real qs{np.round(corr_stopp.stoppingtimes[plot_nn.step+1,:].flatten())}")
         logger.debug(f"approx. qs{np.round(model(input_test).detach().numpy().flatten())}")
         corr_stopp.mistakes += np.sum(np.abs(np.round(corr_stopp.stoppingtimes[plot_nn.step+1,:].flatten())-np.round(model(input_test).detach().numpy().flatten())))
@@ -96,14 +97,14 @@ def update_graph_scatter(x):
     text1 = f"step in the game: {plot_nn.step} \n learning epoch: {plot_nn.epoch} \n loss: {loss} \n number of mistakes {corr_stopp.mistakes}"
 
     #generate copied model 
-    fpath2 = f"/Users/andreferdinand/Desktop/Coding2/output/neural_networkscopy/phase_{plot_nn.step}/model_epoch_{plot_nn.epoch}.pt"
+    fpath2 = f"/Users/andreferdinand/Desktop/Coding2/output/neural_networkseasycopy2/phase_{plot_nn.step}/model_epoch_{plot_nn.epoch}.pt"
     checkpoint2 = torch.load(fpath2)
     model.load_state_dict(checkpoint2['model_state_dict'])
     model.train(False)
     model.double()
     out_data2 = model(input)
     loss2 = checkpoint2['loss']
-    #logger.debug(f"list of q values copied {np.round(model(input_test).detach().numpy().flatten().tolist())}")
+    #logger.debug(f"list of q values copied {np.round(mod   el(input_test).detach().numpy().flatten().tolist())}")
     if plot_nn.epoch ==28:
         logger.debug(f"real qs{np.round(corr_stopp.stoppingtimes[plot_nn.step+1,:].flatten())}")
         logger.debug(f"approx. qs copy{np.round(model(input_test).detach().numpy().flatten())}")
@@ -116,12 +117,14 @@ def update_graph_scatter(x):
             y=list(out_data2.detach().numpy().flatten()),
         )
     )
-    plot_nn.epoch +=1
+
     reward_figure2.update_layout(yaxis_range=[0,1])
     text2 = f"step in the game: {plot_nn.step} \n learning epoch: {plot_nn.epoch} \n loss: {loss2} \n number of mistakes {corr_stopp.mistakes2}"
+    plot_nn.epoch +=1
     return reward_figure1, text1, reward_figure2, text2
     # return {'data': traces}
 
 
 if __name__ == "__main__":
     app.run_server(debug=True, port=8091)
+ 
